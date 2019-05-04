7ba12b777387b8ef83b078960215f063859bc94d
Swiftloke <swiftlokex@gmail.com>
2019-05-04 15:24:34 -0400

    Initial Commit- Add HelloWorldButStupid

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldButStupid
+{
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			const string helloworld = @"int main ( )
+{
+std :: cout << ""SomeString"" ;
+return 0 ;
+}";
+			const string iostream = "#include <iostream>";
+			string[] tokens = helloworld.Split(new char[] { ' ', '\n' });
+
+			Console.Write("Enter a value to transform Hello World into: ");
+			string value = Console.ReadLine();
+			if (value.Length < 4)
+			{
+				Console.WriteLine("Please input a longer value to account for all permutations necessary. Please try again.");
+				Environment.Exit(1);
+			}
+			else if (value.Contains(" "))
+			{
+				Console.WriteLine("You cannot have a space in preprocessor definitions. Please try again.");
+				Environment.Exit(1);
+			}
+			var defines = new List<string>();
+			var permutations = new List<string>();
+			Random charcase = new Random();
+			foreach (var token in tokens)
+			{
+				char[] chars = value.ToCharArray();
+				int attempts = 0;
+				while (permutations.Contains(new string(chars)))
+				{
+					for (int i = 0; i < chars.Length; i++)
+					{
+						bool isUpper = charcase.Next(0, 2) == 1;
+						if (isUpper)
+							chars[i] = Char.ToUpper(chars[i]);
+						else
+							chars[i] = char.ToLower(chars[i]);
+					}
+					attempts++;
+					if (attempts == 50) //We clearly don't have a possible permutation; bail
+					{
+						Console.WriteLine("It appears you have entered a string that is impossible to mutate enough times" +
+							" to create Hello World. Please try again.");
+						Environment.Exit(1);
+					}
+				}
+				permutations.Add(new string(chars));
+				string defined = token;
+				if (defined == "\"SomeString\"") //Need to fix what gets printed
+					defined = $"\"{value}\"";
+				defines.Add($"#define {new string(chars)} {defined}");
+			}
+			var final = new StringBuilder();
+			final.Append(iostream + "\n\n");
+			foreach (var define in defines)
+				final.Append(define + '\n');
+			final.Append('\n');
+			//We should only write 5 at a time, as all of them on one line could get pretty long.
+			int tokenswritten = 0;
+			foreach (var permutation in permutations)
+			{
+				final.Append(permutation + ' ');
+				tokenswritten++;
+				if (tokenswritten == 5)
+				{
+					final.Append('\n');
+					tokenswritten = 0;
+				}
+			}
+			Console.WriteLine(final.ToString());
+			Console.ReadKey();
+		}
+    }
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+﻿# Hello World But Stupid
+Ever wanted to write Hello World for C++, but in a fun, unique way? This program is for you. Baffle your colleagues with this meme of a tool!
+# What Is This Thing?
+Hello World But Stupid transmutates this:
+```cpp
+#include <iostream>
+
+int main ( )
+{
+	std :: cout << "SomeString" ;
+	return 0 ;
+}
+```
+Into this:
+```cpp
+#include <iostream>
+
+#define memes int
+#define Memes main
+#define meMEs (
+#define meMES )
+#define MeMeS {
+#define MeMes std
+#define mEMes ::
+#define MeMES cout
+#define mEMEs <<
+#define MEmES "memes"
+#define mEMES ;
+#define memeS return
+#define meMeS 0
+#define MemES ;
+#define MEmes }
+
+memes Memes meMEs meMES MeMeS
+MeMes mEMes MeMES mEMEs MEmES
+mEMES memeS meMeS MemES MEmes
+```
+In other words, every token in the above hello world snippet is transformed into a ``#define`` as a permutation of the string of your choosing. The string in question can be anything at least 4 characters long to allow for enough permutations.
+Also, it needs to be possible to make enough permutations through uppercasing and lowercasing the string in question, and it cannot contain a space (preprocessor definitions can't have spaces, unfortunately).
+Of course, it does replace what is printed, ``SomeString``, with your value.
+# But why?
+I saw a post on r/ProgrammingHumor, came up with this idea as a result, and thought it would be funny. Credit goes to ``u/x32byTe`` for the inspiring post in question.
+# This is stupid.
+No, actually, it's amazing.
